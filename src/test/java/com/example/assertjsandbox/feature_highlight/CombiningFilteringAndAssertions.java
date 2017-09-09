b304f27596c69c7d13fdbf6a4f9f99c861725d41
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2017-09-09 09:55:26 +0900

    Combining filtering and assertions on iterables or arrays

--- src/test/java/com/example/assertjsandbox/feature_highlight/CombiningFilteringAndAssertions.java
+++ src/test/java/com/example/assertjsandbox/feature_highlight/CombiningFilteringAndAssertions.java
@@ -1,6 +1,7 @@
 package com.example.assertjsandbox.feature_highlight;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Condition;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -9,7 +10,7 @@ import java.util.List;
 public class CombiningFilteringAndAssertions {
 
 	/**
-	 * リストの中で、nameに「e」を含んでいるものを抽出
+	 * brandsの中で、nameに「e」を含んでいるものを抽出
 	 */
 	@Test
 	public void filteringPredicate() {
@@ -18,7 +19,6 @@ public class CombiningFilteringAndAssertions {
 		Brand sneeuw = new Brand("sneeuw", "Yukiura", Gender.WOMAN);
 		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
 		Brand dulcamara = new Brand("Dulcamara", "Yoda", Gender.WOMAN);
-
 		List<Brand> brands = Arrays.asList(stof, bedsidedrama, sneeuw, ethosens, dulcamara);
 
 		Assertions.assertThat(brands)
@@ -26,5 +26,113 @@ public class CombiningFilteringAndAssertions {
 				.containsOnly(bedsidedrama, sneeuw);
 	}
 
+	/**
+	 * brandsの中でgenderがMANのものだけ抽出
+	 */
+	@Test
+	public void filteringMatchPropertyValues() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand bedsidedrama = new Brand("bedsidedrama", "Tanita", Gender.MAN);
+		Brand sneeuw = new Brand("sneeuw", "Yukiura", Gender.WOMAN);
+		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
+		Brand dulcamara = new Brand("Dulcamara", "Yoda", Gender.WOMAN);
+		List<Brand> brands = Arrays.asList(stof, bedsidedrama, sneeuw, ethosens, dulcamara);
 
+		Assertions.assertThat(brands)
+				.filteredOn("gender", Gender.MAN)
+				.containsOnly(stof, bedsidedrama, ethosens);
+	}
+
+	/**
+	 * brandsの中でgenderがMANでないものだけ抽出
+	 */
+	@Test
+	public void filteringNotMatchPropertyValues() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand bedsidedrama = new Brand("bedsidedrama", "Tanita", Gender.MAN);
+		Brand sneeuw = new Brand("sneeuw", "Yukiura", Gender.WOMAN);
+		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
+		Brand dulcamara = new Brand("Dulcamara", "Yoda", Gender.WOMAN);
+		List<Brand> brands = Arrays.asList(stof, bedsidedrama, sneeuw, ethosens, dulcamara);
+
+		Assertions.assertThat(brands)
+				.filteredOn("gender", Assertions.not(Gender.MAN))
+				.containsOnly(sneeuw, dulcamara);
+	}
+
+	/**
+	 * brandsの中でnameに"sneeuw", "Dulcamara"を含むものだけ抽出
+	 */
+	@Test
+	public void filteringIncludePropertyValues() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand bedsidedrama = new Brand("bedsidedrama", "Tanita", Gender.MAN);
+		Brand sneeuw = new Brand("sneeuw", "Yukiura", Gender.WOMAN);
+		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
+		Brand dulcamara = new Brand("Dulcamara", "Yoda", Gender.WOMAN);
+		List<Brand> brands = Arrays.asList(stof, bedsidedrama, sneeuw, ethosens, dulcamara);
+
+		Assertions.assertThat(brands)
+				.filteredOn("name", Assertions.in("sneeuw", "Dulcamara"))
+				.containsOnly(dulcamara, sneeuw);
+	}
+
+	/**
+	 * brandsの中でnameに"sneeuw", "Dulcamara"を含まないものだけ抽出
+	 */
+	@Test
+	public void filteringNoIncludePropertyValues() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand bedsidedrama = new Brand("bedsidedrama", "Tanita", Gender.MAN);
+		Brand sneeuw = new Brand("sneeuw", "Yukiura", Gender.WOMAN);
+		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
+		Brand dulcamara = new Brand("Dulcamara", "Yoda", Gender.WOMAN);
+		List<Brand> brands = Arrays.asList(stof, bedsidedrama, sneeuw, ethosens, dulcamara);
+
+		Assertions.assertThat(brands)
+				.filteredOn("name", Assertions.notIn("sneeuw", "Dulcamara"))
+				.containsOnly(stof, bedsidedrama, ethosens);
+	}
+
+	/**
+	 * brandsの中でnameに「e」を含んでいてかつ、genderがMANのものを抽出
+	 */
+	@Test
+	public void filteringMatchMultiplePropertiesValue() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand bedsidedrama = new Brand("bedsidedrama", "Tanita", Gender.MAN);
+		Brand sneeuw = new Brand("sneeuw", "Yukiura", Gender.WOMAN);
+		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
+		Brand dulcamara = new Brand("Dulcamara", "Yoda", Gender.WOMAN);
+		List<Brand> brands = Arrays.asList(stof, bedsidedrama, sneeuw, ethosens, dulcamara);
+
+		Assertions.assertThat(brands)
+				.filteredOn(brand -> brand.getName().contains("e"))
+				.filteredOn("gender", Gender.MAN)
+				.containsOnly(bedsidedrama);
+	}
+
+	/**
+	 * Conditionクラスを使用したフィルタリング
+	 */
+	@Test
+	public void filteringWithCondition() {
+		Condition<Brand> includeEBrands = new Condition<Brand>() {
+			@Override
+			public boolean matches(Brand brand) {
+				return brand.getName().contains("e");
+			}
+		};
+
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand bedsidedrama = new Brand("bedsidedrama", "Tanita", Gender.MAN);
+		Brand sneeuw = new Brand("sneeuw", "Yukiura", Gender.WOMAN);
+		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
+		Brand dulcamara = new Brand("Dulcamara", "Yoda", Gender.WOMAN);
+		List<Brand> brands = Arrays.asList(stof, bedsidedrama, sneeuw, ethosens, dulcamara);
+
+		Assertions.assertThat(brands)
+				.filteredOn(includeEBrands)
+				.containsOnly(bedsidedrama, sneeuw);
+	}
 }
