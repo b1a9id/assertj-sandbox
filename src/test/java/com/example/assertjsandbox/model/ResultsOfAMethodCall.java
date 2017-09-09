b99ff6691e1cc419cf58715952db8f53e613b71a
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2017-09-09 13:56:34 +0900

    Assertions on results of a method call on iterable/array elements

---
+++ src/test/java/com/example/assertjsandbox/model/ResultsOfAMethodCall.java
@@ -0,0 +1,27 @@
+package com.example.assertjsandbox.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ResultsOfAMethodCall {
+
+	/**
+	 * brandsの中からBrand#getNameの結果が一致するnameを抽出
+	 */
+	@Test
+	public void extractingResultOf() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand bedsidedrama = new Brand("bedsidedrama", "Tanita", Gender.MAN);
+		Brand sneeuw = new Brand("sneeuw", "Yukiura", Gender.WOMAN);
+		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
+		Brand dulcamara = new Brand("Dulcamara", "Yoda", Gender.WOMAN);
+		List<Brand> brands = Arrays.asList(stof, bedsidedrama, sneeuw, ethosens, dulcamara);
+
+		Assertions.assertThat(brands)
+				.extractingResultOf("getName")
+				.contains("stof", "bedsidedrama", "sneeuw", "ETHOSENS", "Dulcamara");
+	}
+}
