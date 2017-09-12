ee08f5f0dbad7b0516b211bca8b6002ae18f1f92
Ryosuke Uchitate <u.ryosuke@waja.jp>
2017-09-12 15:14:54 +0900

    Field by field comparisons

---
+++ src/test/java/com/example/assertjsandbox/model/FieldByFieldComparisons.java
@@ -0,0 +1,58 @@
+package com.example.assertjsandbox.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class FieldByFieldComparisons {
+
+	/**
+	 * 全フィールドの値を検証
+	 */
+	@Test
+	public void allFieldAssertion() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand cloneStof = new Brand("stof", "Tanita", Gender.MAN);
+
+		// オブジェクトを比較すると異なる
+		Assertions.assertThat(stof).isNotEqualTo(cloneStof);
+		Assertions.assertThat(stof).isEqualToComparingFieldByField(cloneStof);
+	}
+
+	/**
+	 * 指定したフィールドの値を検証
+	 */
+	@Test
+	public void specificFieldAssertion() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
+		Brand storama = new Brand("storama", "Tanita", Gender.MAN);
+
+		Assertions.assertThat(stof).isEqualToComparingOnlyGivenFields(ethosens, "gender");
+		Assertions.assertThat(stof).isEqualToComparingOnlyGivenFields(storama, "designer", "gender");
+	}
+
+	/**
+	 * 指定したフィールド以外の値を検証
+	 */
+	@Test
+	public void ignoreSpecificFieldAssertion() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand ethosens = new Brand("ETHOSENS", "Hashimoto", Gender.MAN);
+		Brand storama = new Brand("storama", "Tanita", Gender.MAN);
+
+		Assertions.assertThat(stof).isEqualToIgnoringGivenFields(ethosens, "name", "designer");
+		Assertions.assertThat(stof).isEqualToIgnoringGivenFields(storama, "name");
+	}
+
+	/**
+	 * nullのフィールドは無視して検証
+	 */
+	@Test
+	public void ignoreNullFieldAssertion() {
+		Brand stof = new Brand("stof", "Tanita", Gender.MAN);
+		Brand storama = new Brand(null, "Tanita", Gender.MAN);
+
+		// isEqualToIgnoringNullFieldsのstoramaにあるnullであるnameフィールドは無視される
+		Assertions.assertThat(stof).isEqualToIgnoringNullFields(storama);
+	}
+}
